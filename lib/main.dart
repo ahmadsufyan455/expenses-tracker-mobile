50f8bfcec3fbe32872efb5c8218ead4b19203d78
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-09-27 23:21:33 +0700

    feat: add sentry log

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker_mobile/core/di/di.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'app/app.dart';
 
@@ -8,5 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'assets/env/.env');
   await configureDependencies();
-  runApp(const ExpenseTrackerApp());
+  await SentryFlutter.init((options) {
+    options.dsn = dotenv.env['SENTRY_DSN'];
+  }, appRunner: () => runApp(const ExpenseTrackerApp()));
 }
