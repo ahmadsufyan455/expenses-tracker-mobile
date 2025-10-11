2c42d5091d5f57880f174f49bc26e58bc0bd4516
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-10-11 13:33:34 +0700

    chore: setup launch json && configure sentiry only for production

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker_mobile/core/di/di.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
@@ -9,7 +10,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'assets/env/.env');
   await configureDependencies();
-  await SentryFlutter.init((options) {
-    options.dsn = dotenv.env['SENTRY_DSN'];
-  }, appRunner: () => runApp(const ExpenseTrackerApp()));
+
+  if (kReleaseMode && (dotenv.env['SENTRY_DSN']?.isNotEmpty ?? false)) {
+    await SentryFlutter.init((options) {
+      options.dsn = dotenv.env['SENTRY_DSN'];
+      options.environment = 'production';
+      options.enableAutoSessionTracking = true;
+      options.tracesSampleRate = 0.2;
+    }, appRunner: () => runApp(const ExpenseTrackerApp()));
+  } else {
+    runApp(const ExpenseTrackerApp());
+  }
 }
